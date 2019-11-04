a57fd97a9322c61624801a9b98aa5fe8cbb39a66
Charlie <cr646531@gmail.com>
2019-11-04 04:54:44 -0500

    created redux store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,45 @@
-const LOAD_CARDS = 'LOAD_CARDS';
-
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-
 import axios from 'axios';
+
+// ------------------------- //
+
+const LOAD_CARDS = 'LOAD_CARDS';
+
+// ------------------------- //
+
+const cardsReducer = ( state = [], action )=> {
+    switch(action.type){
+        case LOAD_CARDS:
+            state = action.cards;
+            break;
+    }
+    return state;
+};
+
+// ------------------------- //
+
+const reducer = combineReducers({
+    cards: cardsReducer
+});
+
+export default createStore(reducer, applyMiddleware(thunk));
+
+// ------------------------- //
+
+const _loadCards = (cards)=> ({
+    type: LOAD_CARDS,
+    cards
+});
+
+// ------------------------- //
+
+const loadCards = ()=> {
+    return (dispatch)=> {
+        return axios.get('/cards')
+            .then(res => res.data)
+            .then(cards => dispatch(_loadCards(cards)));
+    };
+};
+
+export { loadCards };
