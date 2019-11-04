b5e88b968c197dc8487c4207e32818c3cdd7aca3
Charlie <cr646531@gmail.com>
2019-11-04 03:22:45 -0500

    reconfigured webpack and babel to work with react

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+const root = document.getElementById('root');
+import App from './App';
+
+render(<App />, root);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    devtool: 'source-map',
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          loader: ['style-loader','css-loader']
-        },
-        {
-          exclude: /node_modules/,
-          test: /\.js$/,
-          loader: 'babel-loader',
-        }
-      ],
-    },
-  };
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ['style-loader','css-loader']
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
