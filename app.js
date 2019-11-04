ee62a45878821ca7ef2b48b7687c2313ea529e76
Charlie <cr646531@gmail.com>
2019-11-04 01:56:48 -0500

    added webpack

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
+const indexFile = path.join(__dirname, 'index.html');
+
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+
+app.get('/', (req, res, next) => res.sendFile(indexFile));
+
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 const app = require('./app');
-const path = require('path');
 const port = process.env.PORT || 3000;
-
-const indexFile = path.join(__dirname, 'index.html');
-
-app.get('/', (req, res, next) => res.sendFile(indexFile));
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
