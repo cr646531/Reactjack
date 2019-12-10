774ea92d7d19ebad5aaa1b9fd4ad474fcb04ecf9
Charlie <cr646531@gmail.com>
2019-12-10 00:26:15 -0500

    changed name of project to Reactjack

--- src/components/Nav/Navbar.js
+++ src/components/Nav/Navbar.js
@@ -44,7 +44,7 @@ export default class Nav extends Component {
         return (
             <div>
                 <nav className="navbar navbar-expand-sm navbar-light bg-light">
-                    <a className="navbar-brand" href="#">Blackjack-react</a>
+                    <a className="navbar-brand" href="#">Reactjack</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
